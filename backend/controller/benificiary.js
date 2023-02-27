a8d2b29b05ec98c9f39ed5254019b81d3992c63a
Saksham511 <sakshamshrestha511@gmail.com>
2023-02-27 15:23:09 +0545

    for backup

--- backend/controller/benificiary.js
+++ backend/controller/benificiary.js
@@ -1,24 +1,34 @@
 import asynchandler from "express-async-handler";
 import Project from "../models/project.js";
 
-export const listProjectByBenificiary= asynchandler((req,res)=>{
-    const{id}=req.params;
-    Project.findById(id)
-    .then((response)=> {
-        const { projectName, projectInfo, startDate, deadline, claimableFund }= response;
-        res.send({
-            "projectName": projectName,
-            "projectInfo": projectInfo,
-            "startDate": startDate,
-            "deadline": deadline,
-            "claimableFund": claimableFund,
-        });
-    })
-    .catch((err)=> {
+export const listProjectByBenificiary= asynchandler(async(req,res)=>{
+    
+    const {username}= req.body;
+
+    try{
+        const project1=await Project.find({})
+        console.log(project1)
+        for (let i=0;i<5;i++)
+            {
+                if(project1[i].benificiaries.username== username)
+                {
+                    const { projectName, projectInfo, startDate, deadline, claimableFund }= project1[i];
+                    res.send({
+                        projectName: projectName,
+                        projectInfo: projectInfo,
+                        startDate: startDate,
+                        deadline: deadline,
+                        claimableFund: claimableFund,
+                    });
+                }
+                
+            }
+    }
+    catch(err){
         res.send({
             message:"Error getting project",
             status: JSON.stringify(err),
         });
         console.log(err);
-    });
+    }
 });
