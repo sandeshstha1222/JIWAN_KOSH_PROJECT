f388c0ca93fb95a1327f71abbfcb65621d79a080
sarojramtel <nepalisaroj056@gmail.com>
2023-03-10 00:04:39 +0545

    initial commit of web3

--- truffle-config.js
+++ truffle-config.js
@@ -41,10 +41,11 @@
  * https://trufflesuite.com/docs/truffle/getting-started/using-the-truffle-dashboard/
  */
 
-// require('dotenv').config();
+// require("dotenv").config();
 // const { MNEMONIC, PROJECT_ID } = process.env;
-
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const MNEMONIC =
+  "hamster intact moment orange prize silk fence tenant fix owner picture notice";
 
 module.exports = {
   /**
@@ -82,13 +83,17 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // goerli: {
-    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
-    //   network_id: 5,       // Goerli's id
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    goerli: {
+      provider: () =>
+        new HDWalletProvider(
+          MNEMONIC,
+          "https://goerli.infura.io/v3/c697c7760a194b26ae6eaf1a5db493c0"
+        ),
+      network_id: 5, // Goerli's id
+      confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
+      // timeoutBlocks: 1200, // # of blocks before a deployment times out  (minimum/default: 50)
+      // skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
     //
     // Useful for private networks
     // private: {
