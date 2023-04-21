f3158911505b6d76b4f0f43278d8a4903f794ddc
sarojramtel <nepalisaroj056@gmail.com>
2023-04-21 07:28:47 +0545

    changed to ganche testnet

--- truffle-config.js
+++ truffle-config.js
@@ -65,11 +65,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //   host: "127.0.0.1", // Localhost (default: none)
-    //   port: 7545, // Standard Ethereum port (default: none)
-    //   network_id: "*", // Any network (default: none)
-    // },
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 7545, // Standard Ethereum port (default: none)
+      network_id: 5777, // Any network (default: none)
+    },
     //
     // An additional network, but with some advanced options…
     // advanced: {
@@ -83,19 +83,19 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    goerli: {
-      provider: () =>
-        new HDWalletProvider(
-          MNEMONIC,
-          "https://goerli.infura.io/v3/c697c7760a194b26ae6eaf1a5db493c0"
-        ),
-      network_id: 5, // Goerli's id
-      // gas: 8000000,
-      // gasPrice: 10000000000,
-      confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
-      // timeoutBlocks: 1200, // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-    },
+    // goerli: {
+    //   provider: () =>
+    //     new HDWalletProvider(
+    //       MNEMONIC,
+    //       "https://goerli.infura.io/v3/c697c7760a194b26ae6eaf1a5db493c0"
+    //     ),
+    //   network_id: 5, // Goerli's id
+    //   // gas: 8000000,
+    //   // gasPrice: 10000000000,
+    //   confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
+    //   // timeoutBlocks: 1200, // # of blocks before a deployment times out  (minimum/default: 50)
+    //   skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    // },
     //
     // Useful for private networks
     // private: {
