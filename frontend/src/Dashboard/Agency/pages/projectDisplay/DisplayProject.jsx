0b7581036829031091058234b7b3e4e911bb10e4
sandeshstha1222 <sananddesh1029@gmail.com>
2023-04-22 14:04:01 +0545

    dislay project

--- frontend/src/Dashboard/Agency/pages/projectDisplay/DisplayProject.jsx
+++ frontend/src/Dashboard/Agency/pages/projectDisplay/DisplayProject.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./displayProject.css";
 import axios from "axios";
-import AgencySidebar from "../../Components/agencysidebar/AgencySidebar";
-import charity from "../../../../images/project.jpg";
 
 const DisplayProject = () => {
   const [mydata, setMyData] = useState([]);
@@ -23,45 +21,38 @@ const DisplayProject = () => {
 
   return (
     <div className="displayProject">
-      <div>
-        <AgencySidebar />
-      </div>
       <div className="grid">
-        <h1 className="displayProjectTitle">Display Project</h1>
+        <h1 className="displayTitle">Display Project</h1>
         {mydata.map((post) => {
           const {
             projectName,
-            numOfBenificiary,
+            numOfBeneficiary,
             projectInfo,
             startDate,
             deadline,
             target,
-            benificiaries,
+            beneficiaries,
           } = post;
           return (
-            <div className="displayProjectItem" key={projectName}>
-              <img style={{ width: "18em" }} src={charity} alt="PROJECT" />
+            <div className="displayItem" key={projectName}>
+              <p className="displayText">Project Name: {projectName}</p>
+              <p className="displayText">
+                Numberofbeneficiary: {numOfBeneficiary}
+              </p>
+              {post.beneficiaries.map((data) => {
+                const { email, username } = data;
+                return (
+                  <div>
+                    Emails :{email} <br />
+                    Username: {username}
+                  </div>
+                );
+              })}
+
               <p className="displayText">Projectinfo: {projectInfo}</p>
-
-              <div className="restdata">
-                <p className="displayText">Project Name: {projectName}</p>
-                <p className="displayText">
-                  Numberofbeneficiary: {numOfBenificiary}
-                </p>
-                {post.benificiaries.map((data) => {
-                  const { email, username } = data;
-                  return (
-                    <div>
-                      Emails :{email} <br />
-                      Username: {username}
-                    </div>
-                  );
-                })}
-
-                <p className="displayText">Startdate: {startDate}</p>
-                <p className="displayText">Deadline: {deadline}</p>
-                <p className="displayText">Target: {target}</p>
-              </div>
+              <p className="displayText">Startdate: {startDate}</p>
+              <p className="displayText">Deadline: {deadline}</p>
+              <p className="displayText">Target: {target}</p>
             </div>
           );
         })}
